4d740fc8cb79f297a9d2b7c2712247b7c778edd6
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-03-11 18:30:38 +0100

    fix import layout in App.jsx + npm issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+//importo la homepage
+import HomePage from './pages/HomePage';
+//importo la pagina di dettaglio dei film
+import MovieInfo from './pages/MovieInfo';
+
+//importo layout
+import DefaultLayout from './layouts/DefaultLayout';
+
+//importo rotte da react-router (radice rotte, blocco rotte, singole rotte)
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <>
-      <h1>MOVIE APP</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          {/* HomePage */}
+          <Route index path="/" element={<HomePage />} />
+          {/* Dettagli film */}
+          <Route path="/movies/:id" element={<MovieInfo />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
