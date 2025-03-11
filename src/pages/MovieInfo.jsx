4e09e4e7a355550e1583ad47ddfb0a0a6f9b92b3
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-03-11 18:40:55 +0100

    improve formatting in MovieInfo.jsx

--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -6,10 +6,10 @@ const MovieInfo = () => {
             <div className="movie-info">
                 <img></img>
                 <h1>titolo film</h1>
-                <span>regista</span>
-                <span>genere</span>
-                <span>anno</span>
-                <span>trama</span>
+                <p>regista</p>
+                <p>genere</p>
+                <p>anno</p>
+                <p>trama</p>
                 <MovieReview />
             </div>
         </>
