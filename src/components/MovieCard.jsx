ceb22eb5c42e2327f9e1a8fdf5a600fb20dc218e
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-03-12 16:16:56 +0100

    complete milestone 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,29 @@
 //importo Link del modulo react-router
 import { Link } from "react-router-dom"
 
-const MovieCard = () => {
+const MovieCard = ({ movieProp }) => {
+    const { id, title, director, genre, release_year, abstract, image } = movieProp;
 
     return (
 
         <div className="movie-card">
-            <Link className="card" to="/"></Link>
-            {/* immagine copertina film statica di prova */}
-            <img className="card-img-top" src="http://localhost:3000/img/movies_cover/inception.jpg" alt="descrizione immagine" />
-            <h5 className="movie-title">Titolo film</h5>
-            <address><i>Regista</i></address>
-            <p className="card-text">Trama</p>
-        </div>
+            <Link className="card" to={`/movies/${id}`}>Dettagli film</Link>
+            {/* dati dinamici */}
 
+            {/* immagine */}
+            <img className="card-img-top" src={image} alt={title} />
+            {/* titolo */}
+            <h5 className="movie-title">{title}</h5>
+            {/* regista */}
+            <address><i>{director}</i></address>
+            {/* genere */}
+            <span>{genre}</span>
+            {/* anno di uscita */}
+            <span>{release_year}</span>
+            {/* trama */}
+            <p className="card-text">{abstract}</p>
+        </div>
     )
 }
+
 export default MovieCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,47 @@
 import MovieCard from "../components/MovieCard"
+//importo axios
+import axios from "axios"
+//uso di state e effecti
+import { useState, useEffect } from "react"
+
 
 const HomePage = () => {
+
+    //setto lo stato del componente
+    const [movies, setMovies] = useState([])
+
+    //funzione di fetching dei dati della lista dei film (INDEX)
+    const fetchMovies = () => {
+        axios.get("http://localhost:3000/api/movies")
+            .then(
+                res => {
+                    setMovies(res.data)
+                }
+            )
+            .catch(err => console.log(err))
+    }
+
+    useEffect(fetchMovies, []);
+
+    //funzione di rendering delle card dei film
+    const renderMovies = () => {
+        return movies.map(
+            movie => {
+                return (
+                    <div className="col" key={movie.id}>
+                        <MovieCard movieProp={movie} />
+                    </div>
+
+                )
+            }
+        )
+    }
+
     return (
         <>
             <h1 className="movies_list">Lista film</h1>
             <div className="movies-container">
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
+                {renderMovies()}
             </div>
         </>
     )
